2a53a361c970df1ba078dfdadfec662df369ba85
wangwencheng <960075207@qq.com>
2020-11-04 17:18:29 +0800

    前端播放器代码优化提交

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,9 @@
 import React, {Component, Fragment} from 'react';
 import {connect} from 'dva';
-import {SearchBar, Modal, ListView} from 'antd-mobile';
+import {Toast, SearchBar, Modal, ListView} from 'antd-mobile';
 import {videoInfo} from "../../services/video";
-import Link from 'umi/link';
 import router from 'umi/router';
 import styles from './index.less';
-import ReactDOM from 'react-dom';
-import $ from 'jquery'
 import {
   Player,
   ControlBar,
@@ -30,79 +27,78 @@ function MyBody(props) {
   );
 }
 
-const NUM_SECTIONS = 5;
-const NUM_ROWS_PER_SECTION = 5;
-let pageIndex = 0;
-
-const dataBlobs = {};
-let sectionIDs = [];
-let rowIDs = [];
-
-function genData(pIndex = 0) {
-  for (let i = 0; i < NUM_SECTIONS; i++) {
-    const ii = (pIndex * NUM_SECTIONS) + i;
-    const sectionName = `Section ${ii}`;
-    sectionIDs.push(sectionName);
-    dataBlobs[sectionName] = sectionName;
-    rowIDs[ii] = [];
-
-    for (let jj = 0; jj < NUM_ROWS_PER_SECTION; jj++) {
-      const rowName = `S${ii}, R${jj}`;
-      rowIDs[ii].push(rowName);
-      dataBlobs[rowName] = rowName;
-    }
-  }
-  sectionIDs = [...sectionIDs];
-  rowIDs = [...rowIDs];
-}
 
 @connect(({home}) => ({home}))
 class Home extends Component {
   constructor(props) {
     super(props);
-    const getSectionData = (dataBlob, sectionID) => dataBlob[sectionID];
-    const getRowData = (dataBlob, sectionID, rowID) => dataBlob[rowID];
     const dataSource = new ListView.DataSource({
-      getRowData,
-      getSectionHeaderData: getSectionData,
       rowHasChanged: (row1, row2) => row1 !== row2,
-      sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
     });
 
     this.state = {
       dataSource,
       isLoading: true,
-      height: document.documentElement.clientHeight * 3 / 4,
-      banner: [],
-      icon: [],
-      tab: [],
       videoList: [],
-      bottomBanner: [],
+      pageNo: 1,
+      pageSize: 2,
+      hasMore: true,
+      refreshing: true,
       buttonMenuList: [],
-      cardLists: [],
-      tofobanner: [],
       visible: false,
     }
   }
 
   componentDidMount() {
-    var buttonDom = document.getElementsByClassName("video-react-big-play-button video-react-big-play-button-left");
-    console.log('buttonDom',buttonDom)
+    this.getData(true)
+  }
 
+  getData(ref = false) {
+    //获取数据
+    const para = {}
+    para.pageSize = this.state.pageSize
+    para.pageNo = this.state.pageNo
+    videoInfo(para).then((result) => {
+      if (result && result.code == 0) {
+        const dataList = result.data.records
+        const len = dataList.length
+        if (len <= 0) {                                // 判断是否已经没有数据了
+          this.setState({
+            refreshing: false,
+            isLoading: false,
+            hasMore: false
+          })
+          Toast.info('没有数据了~', 1)
+          return false
+        }
 
-    // you can scroll to the specified position
-    // setTimeout(() => this.lv.scrollTo(0, 120), 800);
-    const hei = document.documentElement.clientHeight - ReactDOM.findDOMNode(this.lv).parentNode.offsetTop;
-    // simulate initial Ajax
-    setTimeout(() => {
-      genData();
-      this.setState({
-        buttonMenuList: this.props.home.buttonMenuList,
-        dataSource: this.state.dataSource.cloneWithRowsAndSections(dataBlobs),
-        isLoading: true,
-        height: hei,
-      });
-    }, 600);
+        if (ref) {
+          //这里表示刷新使用
+          // 下拉刷新的情况，重新添加数据即可(这里等于只直接用了第一页的数据)
+          this.setState({
+            pageNo: this.state.pageNo,
+            dataSource: this.state.dataSource.cloneWithRows(dataList), // 数据源中的数据本身是不可修改的,要更新datasource中的数据，请（每次都重新）调用cloneWithRows方法
+            hasMore: true, // 下拉刷新后，重新允许开下拉加载
+            refreshing: false, // 是否在刷新数据
+            isLoading: false, // 是否在加载中
+            videoList: dataList // 保存数据进state，在下拉加载时需要使用已有数据
+          })
+        } else {
+          // 这里表示上拉加载更多
+          // 合并state中已有的数据和新增的数据
+          const dataArr = this.state.videoList.concat(dataList) //关键代码
+          this.setState({
+            pageNo: this.state.pageNo,
+            dataSource: this.state.dataSource.cloneWithRows(dataArr), // 数据源中的数据本身是不可修改的,要更新datasource中的数据，请（每次都重新）调用cloneWithRows方法
+            refreshing: false,
+            isLoading: false,
+            videoList: dataArr // 保存新数据进state
+          })
+        }
+
+      }
+    })
+    console.log('getData')
   }
 
   getVideoInfo = (params) => {
@@ -116,32 +112,33 @@ class Home extends Component {
   }
 
 
-  makeButtonCenter() {
-    // const dom = ReactDOM.findDOMNode("button");
-    // console.log('dom',dom)
-    // const html = ReactDOM.findDOMNode("button");
-    // console.log(html)
-  }
-
   componentWillMount() {
     this.getVideoInfo();
   }
 
+  // 滑动到底部时加载更多
   onEndReached = (event) => {
-    // load new data
-    // hasMore: from backend data, indicates whether it is the last page, here is false
-    if (this.state.isLoading && !this.state.hasMore) {
-      return;
+    // 加载中或没有数据了都不再加载
+    if (this.state.isLoading || !this.state.hasMore) {
+      return
     }
-    console.log('reach end', event);
-    this.setState({isLoading: true});
-    setTimeout(() => {
-      genData(++pageIndex);
-      this.setState({
-        dataSource: this.state.dataSource.cloneWithRowsAndSections(dataBlobs),
-        isLoading: false,
-      });
-    }, 1000);
+    this.setState({
+      isLoading: true,
+      pageNo: this.state.pageNo + 1, // 加载下一页
+    }, () => {
+      this.getData(false)
+    })
+  }
+
+  // 下拉刷新
+  onRefresh = () => {
+    this.setState({
+      refreshing: true,
+      isLoading: true,
+      pageNo: 1 // 刷新嘛，一般加载第一页，或者按你自己的逻辑（比如每次刷新，换一个随机页码）
+    }, () => {
+      this.getData(true)
+    })
   }
 
   buttonLink(link) {
@@ -181,13 +178,7 @@ class Home extends Component {
       </div>
     );
 
-    let data = this.state.videoList;
-    let index = data.length - 1;
     const row = (rowData, sectionID, rowID) => {
-      if (index < 0) {
-        index = data.length - 1;
-      }
-      const obj = data[index--];
       return (
         <Fragment>
           <div key={rowID} className='videoDiv'>
@@ -199,11 +190,11 @@ class Home extends Component {
               width={'100%'}
               height={298}
               //  preload={'metadata'}
-              poster={obj.videoThumbnail}
+              poster={rowData.videoThumbnail}
               //   aspectRatio={'1:1'}     //视频宽高比
             >
               <source
-                src={obj.videoUrl}
+                src={rowData.videoUrl}
                 type="video/mp4"
               />
               <ControlBar autoHide={false} disableDefaultControls={false}>
